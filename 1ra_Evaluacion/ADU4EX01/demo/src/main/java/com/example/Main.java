5241456e77f54f89ecb46b684d782a94de153f53
Jonathan <jbarragandela@nigul.cide.es>
2024-12-11 20:39:54 +0100

    gg

---
+++ 1ra_Evaluacion/ADU4EX01/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,60 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        
+    }
+}
+
+
+/*
+• Kilometraje: 54.000 km. Año: 2017. 120cv / 62Nm Torque.
+• Motor 4 cilindros en línea, 599cc.
+• Electrónica: Control de tracción 6 niveles y OFF,
+  3 mapas de motor A (Full), B (lluvia) y Standard. ABS, Quickshifter para subir de marchas.
+• Suspensiones 43mm KYB de la R1, ajustables en precarga, rebote y compresión tanto delanteras como trasera.
+• Embrague antirrebote.
+• Frenada delantera de la R1: Pinzas Nissin radiales 4 pistones, discos flotantes 320 mm.
+  Frenada trasera disco de 220mm.
+• Depósito de 17l.
+• Peso: 189kg.
+• Neumáticos Bridgestone S22R. A más del 70% de vida útil.
+
+Extras:
+• Escape Akrapovic Homologado y diseñado para esta R6 con logo a láser (900+€) se entrega original.
+• DB Killer opcional (70€), Puedes circular sin él legalmente, pero lo compré para asegurarme.
+• Supresor de catalizador (130€), va de serie ahora mismo.
+• Cúpula ahumada Puig (120€), monta la de serie ahora mismo.
+• Portamatriculas Corto Puig (80€), entrego el de serie también.
+• Intermitentes led traseros integrados (30€) doy los de serie también.
+• Protector de depósito y tapa de depósito de carbono (20€).
+• Grips laterales Stompgrip transparentes y negros en pack de 4 (70€).
+• Protectores laterales/sliders Yamaha oficiales (160€).
+• Protectores de basculante y suspensiones delanteras (60€).
+• Contrapesos Puig (60€).
+• Manetas Puig (180€), doy las de serie también.
+• Puños Domino de competición grises (20€) monta originales ahora mismo.
+• Colín monoplaza (30€), también tengo el asiento trasero.
+• Protecciones de motor GB Racing (240€).
+• Faro trasero led oscuro homologado (150€).
+• Depositos de líquido de freno Brembo (20€).
+• Alarma (40€) con 2 mandos.
+
+Estado:
+La moto ha sido muy mimada, siempre en garaje y sin una sola caída, se han hecho tantos km por usarla a diario por autopista para ir a estudiar.
+Respetando los tiempos de calentamiento en todas ocasiones.
+Cambios de aceite hechos a rajatabla, con sus cambios de filtro.
+Líquido refrigerante recién cambiado.
+Filtro de aire BMC de alto flujo reutilizable.
+Pastillas de freno nuevas, discos nuevos.
+Kit de arrastre como nuevo y bien mantenido.
+Reglaje de válvulas realizado a los 48.000km.
+ITV recién pasada sin problemas a la primera.
+La moto cuenta con las 2 llaves negras + la llave roja.
+
+Precio:
+13.500€, Precio final con todos los extras NO negociable (solo los extras valen mas de 2.000€), no encontrarás una con tantos extras y tan cuidada como esta.
+No respondo a ofertas absurdas. No cambios.
+*/
